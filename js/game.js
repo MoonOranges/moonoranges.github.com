a469e758288e28025eaeadaa9cf8af6812e7346d
MoonOranges <Robbieextreme99@outlook.com>
2023-02-14 09:44:00 +1100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -11,7 +11,7 @@ var bgImage = new Image();
 bgImage.onload = function () {
 	bgReady = true;
 };
-bgImage.src = "images/classroom.png";
+bgImage.src = "images/classroom.jpeg";
 
 // Hero image
 var heroReady = false;
@@ -103,7 +103,7 @@ var render = function () {
 	ctx.font = "24px Helvetica";
 	ctx.textAlign = "left";
 	ctx.textBaseline = "top";
-	ctx.fillText("Memes Destroyed: " + monstersCaught, 32, 32);
+	ctx.fillText("Enemies Destroyed: " + monstersCaught, 32, 32);
 };
 
 // The main game loop
